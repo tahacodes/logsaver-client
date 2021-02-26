8245986af880e6b8eb9ed64fe22f6636bd755324
Taha Farahani <iotahacodes@gmail.com>
2021-02-26 14:33:04 +0330

    options

--- main.sh
+++ main.sh
@@ -1,21 +1,36 @@
-while true; 
-do
-  seed=$(((RANDOM%10)+1))
-  if [ $(($seed%2)) -eq 0 ]
-  then
-    res=$(curl -I rockandcode.ir/first | tr '\r\n' ' ' | tr '\r,' ' ')
-  else
-    res=$(curl -I rockandcode.ir/second | tr '\r\n' ' ' | tr '\r,' ' ')
-  fi
+#!/bin/bash
 
-  timestamp=$(date +%s)
-  log=""
-  if [[ "$res" =~ .*"200 OK".* ]];
-  then
-    log+="OK,$timestamp,$res"
-  else
-    log+="ERR,$timestamp,$res"
-  fi
+server="http://rockandcode.ir"
+logfile="/home/$USER/logs"
 
-  echo $log >> ~/logs
-done
+if [ -n "$1" ]; then
+  if [ "$1" = "save" ]; then
+    if test -f "$logfile"; then
+      curl $server/logsaver -H "Content-type: multipart/form-data" -F file=@$logfile
+    else
+      echo "$logfile doesn't exist."
+    fi
+  elif [ "$1" = "show" ]; then
+    curl $server/logsaver
+  fi
+else
+  while true; 
+  do
+    seed=$(((RANDOM%10)+1))
+    if [ $(($seed%2)) -eq 0 ]; then
+      res=$(curl -I $server/first | tr '\r\n' ' ' | tr '\r,' ' ')
+    else
+      res=$(curl -I $server/second | tr '\r\n' ' ' | tr '\r,' ' ')
+    fi
+
+    timestamp=$(date +%s)
+    log=""
+    if [[ "$res" =~ .*"200 OK".* ]]; then
+      log+="OK,$timestamp,$res"
+    else
+      log+="ERR,$timestamp,$res"
+    fi
+
+    echo $log >> ~/logs
+  done
+fi
